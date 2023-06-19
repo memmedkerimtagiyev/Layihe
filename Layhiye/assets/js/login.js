e797eae9febe3fdcf9c8645c86930db606abbf58
memmedkerimtagiyev <7M16ZU6@code.edu.az>
2023-06-19 22:40:20 +0400

    layihe19

--- Layhiye/assets/js/login.js
+++ Layhiye/assets/js/login.js
@@ -3,7 +3,6 @@ const register_btn = document.getElementById("register_btn");
 const btn = document.getElementById("btn");
 const login = document.getElementById("Login");
 const register = document.getElementById("Register");
-let logArr = [];
 
 register_btn.onclick = function () {
   login.style.left = "-400px";
@@ -24,17 +23,49 @@ const inp_log_user = document.getElementById("inp_log_user");
 
 const inp_log_password = document.getElementById("inp_log_password");
 
-const submit_btn = document.querySelector(".submit_btn");
+const inp_login_btn = document.getElementById("inp_login_btn");
+console.log(inp_login_btn);
 
 function getUser() {
   fetch("http://localhost:3000/users")
     .then((res) => res.json())
     .then((data) => {
-      if (data.find((x) =>x.username === inp_log_user.value && x.parol === inp_log_password) === undefined) {
-        logArr.push(data)
-      }
-      localStorage.setItem("login", JSON.stringify(logArr));
+      // if (data.find((x) =>x.username === inp_log_user.value && x.parol === inp_log_password) === undefined) {
+      //   logArr.push(data)
+      // }
+      // localStorage.setItem("login", JSON.stringify(logArr));
+      data.forEach((element) => {
+        if(inp_log_user.value==="admin" && inp_log_password==="adminadmin"){
+          localStorage.setItem(
+            "login",
+            JSON.stringify({
+              username: element.username,
+              password: element.password,
+            })
+          );
+          window.location.href="admin.html"
+        }
+       else if (
+          element.username === inp_log_user.value &&
+          element.password === inp_log_password.value
+        ) {
+          localStorage.setItem(
+            "login",
+            JSON.stringify({
+              username: element.username,
+              password: element.password,
+            })
+          );
+           
+         window.location.href="home.html"
+        }
+       
+      });
     });
 }
-getUser();
-console.log(logArr);
+
+inp_login_btn.addEventListener("click", (e) => {
+  e.preventDefault()
+  console.log("salam");
+  getUser();
+});
--- Layhiye/assets/js/register.js
+++ Layhiye/assets/js/register.js
@@ -6,70 +6,39 @@ const inp_reg_password = document.getElementById("inp_reg_password");
 
 const inp_reg_email = document.getElementById("inp_reg_email");
 
-// let logArr = [];
-
-// if (JSON.parse(localStorage.getItem("Data")) !== null) {
-//   logArr = JSON.parse(localStorage.getItem("Data"));
-// }
-
-// inp_register_btn.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const obj = {
-//     username: inp_reg_user.value,
-//     password: inp_reg_password.value,
-//     email: inp_log_email.value,
-//   };
-
-//   logArr.push(obj);
 
 
-//   let result1 = false;
-//   console.log(logArr);
-//   if (JSON.parse(localStorage.getItem("Data")) !== null) {
-//     for (let i = 0; i < JSON.parse(localStorage.getItem("Data")).length; i++) {
-//       console.log(JSON.parse(localStorage.getItem("Data"))[i].username);
-//       if (JSON.parse(localStorage.getItem("Data"))[i].username === inp_reg_user.value) {
-//         result1 = true;
-//       } else {
-//         result1 = false;
-//       }
-//     }
-//   } else {
-//     localStorage.setItem("Data", JSON.stringify(arr));
-//   }
-// });
 
+function getUsers() {
+  fetch("http://localhost:3000/users")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
 
-// function getUsers() {
-//   fetch("http://localhost:3000/users")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//     });
-// }
+inp_register_btn.onclick = (e) => {
+  e.preventDefault();
 
-// inp_register_btn.onclick = (e) => {
-//   e.preventDefault();
+  console.log("salam");
+  console.log(inp_reg_user.value);
 
-//   console.log("salam");
-//   console.log(inp_reg_user.value);
-
-//   fetch("http://localhost:3000/users", {
-//     method: "POST",
-//     headers: {
-//       Accept: "application.json",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       username: inp_reg_user.value,
-//       password: inp_reg_password.value,
-//       email: inp_log_email.value,
-//     }),
-//   })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//       getUsers();
-//     });
-// };
-// getUsers();
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      Accept: "application.json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: inp_reg_user.value,
+      password: inp_reg_password.value,
+      email: inp_reg_email.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      getUsers();
+    });
+};
+getUsers();
