c297839f25837f33a8be74dd300aefe8da643518
memmedkerimtagiyev <7M16ZU6@code.edu.az>
2023-06-19 23:50:38 +0400

    son

--- Layhiye/assets/js/nav.js
+++ Layhiye/assets/js/nav.js
@@ -1,10 +1,21 @@
-// const shopping_number_nav = document.querySelector(".shopping-number")
-// const wishlist_number_nav = document.querySelector(".heart-number")
+const shopping_number_nav = document.querySelector(".shopping-number")
+const wishlist_number_nav = document.querySelector(".heart-number")
 // basketArr = JSON.parse(localStorage.getItem("basket"));
 // wishlistArr = JSON.parse(localStorage.getItem("wishlist"));
 // shopping_number_nav.innerHTML=basketArr.length
 // wishlist_number_nav.innerHTML=wishlistArr.length
-
+window.onload = function () {
+    if (localStorage.getItem("basket") !== null) {
+      basketArr = JSON.parse(localStorage.getItem("basket"));
+      shopping_number_nav.innerText=1+basketArr.length
+      
+    }
+    if (localStorage.getItem("wishlist") !== null) {
+      wishlistArr = JSON.parse(localStorage.getItem("wishlist"));
+      wishlist_number_nav.innerHTML=1+wishlistArr.length
+    }
+   
+  };
 window.addEventListener("scroll",function(){
     const nav=this.document.querySelector("nav")
     nav.classList.toggle("sticky",window.scrollY>0)
